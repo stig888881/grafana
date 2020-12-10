90770b00353db6453cf8fd8ab05f5df0f40c0080
stig888881 <71179046+stig888881@users.noreply.github.com>
2020-12-10 22:39:30 +0300

    Update RefreshPicker.tsx

--- packages/grafana-ui/src/components/RefreshPicker/RefreshPicker.tsx
+++ packages/grafana-ui/src/components/RefreshPicker/RefreshPicker.tsx
@@ -4,7 +4,7 @@ import { SelectOptionItem } from '../Select/Select';
 import { Tooltip } from '../Tooltip/Tooltip';
 import { ButtonSelect } from '../Select/ButtonSelect';
 
-export const offOption = { label: 'Off', value: '' };
+export const offOption = { label: 'Выключить', value: '' };
 export const defaultIntervals = ['5s', '10s', '30s', '1m', '5m', '15m', '30m', '1h', '2h', '1d'];
 
 export interface Props {
